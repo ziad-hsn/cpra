9aef106156109ac3aa814e07c58560e2dff1b57c
ziad <ziad.hassan.dev@gmail.com>
2025-07-09 18:43:50 +0300

    defer mutation to avoid free object issues

--- internal/controller/systems/sys_code.go
+++ internal/controller/systems/sys_code.go
@@ -12,10 +12,10 @@ import (
 	"time"
 )
 
-type dispatchInfo struct {
-	Job   jobs.Job
-	Color string
-}
+//type dispatchInfo struct {
+//	Job   jobs.Job
+//	Color string
+//}
 
 type CodeDispatchSystem struct {
 	JobChan                  chan<- jobs.Job
@@ -32,50 +32,58 @@ func (s *CodeDispatchSystem) Initialize(w controller.CPRaWorld, lock sync.Locker
 }
 
 func (s *CodeDispatchSystem) Update(w controller.CPRaWorld) {
-	// The map now holds the new struct
-	toDispatch := make(map[ecs.Entity]dispatchInfo)
+	// Collect entities and jobs to dispatch
+	type dispatchEntry struct {
+		entity ecs.Entity
+		job    jobs.Job
+		color  string
+	}
+	toDispatch := make([]dispatchEntry, 0)
 	query := s.CodeNeededFilter.Query(w.Mappers.World)
-
 	for query.Next() {
 		entity := query.Entity()
-		needed := query.Get()
-		color := needed.Color
-
+		codeNeeded := (*components.CodeNeeded)(query.Query.Get(ecs.ComponentID[components.CodeNeeded](w.Mappers.World)))
 		var job jobs.Job
-		switch color {
+		switch codeNeeded.Color {
 		case "red":
-			job = w.Mappers.RedCodeJob.Get(entity).Job
+			if w.Mappers.World.Has(entity, ecs.ComponentID[components.RedCode](w.Mappers.World)) {
+				job = (*components.RedCodeJob)(w.Mappers.World.Get(entity, ecs.ComponentID[components.RedCodeJob](w.Mappers.World))).Job
+			}
 		case "green":
-			job = w.Mappers.GreenCodeJob.Get(entity).Job
+			if w.Mappers.World.Has(entity, ecs.ComponentID[components.GreenCode](w.Mappers.World)) {
+				job = (*components.GreenCodeJob)(w.Mappers.World.Get(entity, ecs.ComponentID[components.GreenCodeJob](w.Mappers.World))).Job
+			}
 		case "yellow":
-			job = w.Mappers.YellowCodeJob.Get(entity).Job
+			if w.Mappers.World.Has(entity, ecs.ComponentID[components.YellowCode](w.Mappers.World)) {
+				job = (*components.YellowCodeJob)(w.Mappers.World.Get(entity, ecs.ComponentID[components.YellowCodeJob](w.Mappers.World))).Job
+			}
 		case "cyan":
-			job = w.Mappers.CyanCodeJob.Get(entity).Job
+			if w.Mappers.World.Has(entity, ecs.ComponentID[components.CyanCode](w.Mappers.World)) {
+				job = (*components.CyanCodeJob)(w.Mappers.World.Get(entity, ecs.ComponentID[components.CyanCodeJob](w.Mappers.World))).Job
+			}
 		case "gray":
-			job = w.Mappers.GrayCodeJob.Get(entity).Job
+			if w.Mappers.World.Has(entity, ecs.ComponentID[components.GrayCode](w.Mappers.World)) {
+				job = (*components.GrayCodeJob)(w.Mappers.World.Get(entity, ecs.ComponentID[components.GrayCodeJob](w.Mappers.World))).Job
+			}
 		default:
-			// Skip unknown colors
+			log.Printf("Unknown color %q for entity %v", codeNeeded.Color, entity)
 			continue
 		}
-
-		// Store both the job and the color string
-		toDispatch[entity] = dispatchInfo{Job: job, Color: color}
-	}
-	//s.lock.Lock()
-	for entity, job := range toDispatch {
-		select {
-		case s.JobChan <- job.Job:
-
-			w.Mappers.World.ExchangeFn(entity, []ecs.ID{ecs.ComponentID[components.CodePending](w.Mappers.World)}, []ecs.ID{ecs.ComponentID[components.CodeNeeded](w.Mappers.World)}, func(entity ecs.Entity) {
-				code := w.Mappers.CodePending.Get(entity)
-				code.Color = job.Color
-			})
-			// mark entity as pending (using Exchange after a query)
-		default:
-			// handle worker pool full, maybe log or retry
+		if job != nil {
+			toDispatch = append(toDispatch, dispatchEntry{entity: entity, job: job, color: codeNeeded.Color})
 		}
 	}
-	//s.lock.Unlock()
+
+	// Process collected entities
+	for _, entry := range toDispatch {
+		select {
+		case s.JobChan <- entry.job:
+			log.Printf("Sent %s code job for entity %v", entry.color, entry.entity)
+			w.Mappers.World.Exchange(entry.entity, []ecs.ID{ecs.ComponentID[components.CodePending](w.Mappers.World)}, []ecs.ID{ecs.ComponentID[components.CodeNeeded](w.Mappers.World)})
+		default:
+			log.Printf("Job channel full for entity %v", entry.entity)
+		}
+	}
 }
 
 // PulseResultSystem --- RESULT PROCESS SYSTEM ---
@@ -91,49 +99,54 @@ func (s *CodeResultSystem) Initialize(w controller.CPRaWorld, lock sync.Locker) 
 }
 
 func (s *CodeResultSystem) Update(w controller.CPRaWorld) {
+	// Collect results to process
 
+	toProcess := make([]resultEntry, 0)
+loop:
 	for {
-		//s.lock.Lock()
 		select {
 		case res := <-s.ResultChan:
-
-			entity := res.Entity()
-
-			c := w.Mappers.CodePending.Get(entity)
-			var status components.CodeStatusAccessor
-			if c.Color == "" {
-				log.Fatal(GetEntityComponents(w.Mappers.World, entity))
-			}
-			switch c.Color {
-			case "red":
-				_, status = w.Mappers.RedCodeConfig.Get(entity)
-			case "green":
-				_, status = w.Mappers.GreenCodeConfig.Get(entity)
-			case "yellow":
-				_, status = w.Mappers.YellowCodeConfig.Get(entity)
-			case "cyan":
-				_, status = w.Mappers.CyanCodeConfig.Get(entity)
-			case "gray":
-				_, status = w.Mappers.GrayCodeConfig.Get(entity)
-			default:
-				log.Fatal(GetEntityComponents(w.Mappers.World, entity))
-			}
-			name := w.Mappers.Name.Get(entity)
-			if res.Error() != nil {
-				status.SetFailure(res.Error())
-				// Re-acquire Name mapper if it's dynamic or might be affected by prior changes, though unlikely for Name
-
-				fmt.Printf("Monitor %s Code failed\n", *name)
-
-			} else {
-				status.SetSuccess(time.Now())
-				fmt.Printf("Monitor %s %q code sent successfully\n", *name, c.Color)
-			}
-			w.Mappers.CodePending.Remove(entity)
-			//s.lock.Unlock()
+			toProcess = append(toProcess, resultEntry{entity: res.Entity(), result: res})
 		default:
-			//s.lock.Unlock()
-			return
+			break loop // Exit loop when no more results
 		}
+	}
+
+	// Process collected results
+	for _, entry := range toProcess {
+		entity := entry.entity
+		res := entry.result
+
+		// Get all component pointers before structural changes
+		codePending := w.Mappers.CodePending.Get(entity)
+		name := (*components.Name)(w.Mappers.World.Get(entity, ecs.ComponentID[components.Name](w.Mappers.World)))
+		var status components.CodeStatusAccessor
+		switch codePending.Color {
+		case "red":
+			_, status = w.Mappers.RedCodeConfig.Get(entity)
+		case "green":
+			_, status = w.Mappers.GreenCodeConfig.Get(entity)
+		case "yellow":
+			_, status = w.Mappers.YellowCodeConfig.Get(entity)
+		case "cyan":
+			_, status = w.Mappers.CyanCodeConfig.Get(entity)
+		case "gray":
+			_, status = w.Mappers.GrayCodeConfig.Get(entity)
+		default:
+			log.Printf("Unknown color %q for entity %v", codePending.Color, entity)
+			continue
+		}
+
+		// Update data
+		if res.Error() != nil {
+			status.SetFailure(res.Error())
+			fmt.Printf("Monitor %s Code failed\n", *name)
+		} else {
+			status.SetSuccess(time.Now())
+			fmt.Printf("Monitor %s %q code sent successfully\n", *name, codePending.Color)
+		}
+
+		// Perform structural change last
+		w.Mappers.CodePending.Remove(entity)
 	}
 }
